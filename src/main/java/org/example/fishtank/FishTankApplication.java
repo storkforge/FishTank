931626dd17d68f85a35e6eefbc9417bf46625ee3
Tova <tova.bryngelsson@live.se>
2025-03-19 10:55:18 +0100

    initial commit

---
+++ src/main/java/org/example/fishtank/FishTankApplication.java
@@ -0,0 +1,13 @@
+package org.example.fishtank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishTankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FishTankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/fishtank/FishTankApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fishtank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishTankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
