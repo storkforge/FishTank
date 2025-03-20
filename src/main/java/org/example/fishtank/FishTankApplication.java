a89e4f102be56915ce6eeae00b3f2a9c2137a6ee
VictoriaNaenfeldtEklund <victoria.naenfeldt.eklund@iths.se>
2025-03-20 15:19:40 +0100

    Test comment

--- src/main/java/org/example/fishtank/FishTankApplication.java
+++ src/main/java/org/example/fishtank/FishTankApplication.java
@@ -3,6 +3,7 @@ package org.example.fishtank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// TEST COMMENT
 @SpringBootApplication
 public class FishTankApplication {
 //
