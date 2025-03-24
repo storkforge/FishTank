cfe16e97dad3d6d8cf1755b14339c6fc9b7b7f3b
Vilhelm Kennedal <vilhelm.kennedal@iths.se>
2025-03-24 14:13:50 +0100

    add sexTable, migration and entity

---
+++ src/main/java/org/example/fishtank/domain/entity/Sex.java
@@ -0,0 +1,33 @@
+package org.example.fishtank.domain.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+@Table(name = "sex")
+public class Sex {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, length = Integer.MAX_VALUE)
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
