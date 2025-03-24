ff23204642a0339899a681d4f1c144d6342a370c
jesper <Jesper.Sandklef@iths.se>
2025-03-24 13:50:30 +0100

    Added fishRepository

--- src/main/java/org/example/fishtank/infrastructure/persistence/UserRepository.java
+++ src/main/java/org/example/fishtank/infrastructure/persistence/UserRepository.java
@@ -1,9 +1,10 @@
 package org.example.fishtank.infrastructure.persistence;
 
+import org.example.fishtank.domain.entity.AppUser;
 import org.example.fishtank.domain.entity.Fish;
 import org.springframework.data.repository.CrudRepository;
 
-public interface FishRepository extends CrudRepository <Fish, Integer> {
+public interface UserRepository extends CrudRepository <AppUser, Integer> {
 
 
 }
