59b55d94289f905eefbe2011499584f9fe25f628
JesperSandklef <jesper.sandklef@iths.se>
2025-03-24 13:54:43 +0100

    Added fishRepository (#20)
    * Added fishRepository
    
    * Added fishRepository
    
    * Added userRepository

---
+++ src/main/java/org/example/fishtank/infrastructure/persistence/UserRepository.java
@@ -0,0 +1,8 @@
+package org.example.fishtank.infrastructure.persistence;
+
+import org.example.fishtank.domain.entity.AppUser;
+import org.example.fishtank.domain.entity.Fish;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository <AppUser, Integer> {
+}
