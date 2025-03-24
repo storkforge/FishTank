cc8c1ed45f48f651cf80b4a81a288128e493db12
jesper <Jesper.Sandklef@iths.se>
2025-03-24 09:58:26 +0100

    test branch

--- src/main/java/org/example/fishtank/FishTankApplication.java
+++ src/main/java/org/example/fishtank/FishTankApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FishTankApplication {
-//
+
     public static void main(String[] args) {
         SpringApplication.run(FishTankApplication.class, args);
     }
