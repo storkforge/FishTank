620bc5e6b8375c435cf94e270837d678201de734
Oskar Dahlström <0skar01@hotmail.se>
2025-03-25 11:14:23 +0100

    implements update and delete functionality

--- src/main/java/org/example/fishtank/infrastructure/persistence/FishRepository.java
+++ src/main/java/org/example/fishtank/infrastructure/persistence/FishRepository.java
@@ -3,5 +3,9 @@ package org.example.fishtank.infrastructure.persistence;
 import org.example.fishtank.domain.entity.Fish;
 import org.springframework.data.repository.ListCrudRepository;
 
+
+
 public interface FishRepository extends ListCrudRepository<Fish, Integer> {
+
+
 }
