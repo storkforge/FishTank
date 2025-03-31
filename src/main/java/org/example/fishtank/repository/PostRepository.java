1cc290213b5537981e4d7f453cb2bd63165f5553
Tova <tova.bryngelsson@live.se>
2025-03-31 15:27:41 +0200

    repositories but right this time

--- src/main/java/org/example/fishtank/repository/PostRepository.java
+++ src/main/java/org/example/fishtank/repository/PostRepository.java
@@ -3,5 +3,5 @@ package org.example.fishtank.repository;
 import org.example.fishtank.model.entity.Post;
 import org.springframework.data.repository.ListCrudRepository;
 
-public interface PostRepository extends ListCrudRepository<Post, Integer> {
+public interface PostRepository extends ListCrudRepository<AppUser, Integer> {
 }
