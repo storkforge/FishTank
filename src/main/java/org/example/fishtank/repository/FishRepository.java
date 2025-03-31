740f552b5c299198d25ac8f68f229127768d01ab
Oskar Dahlström <0skar01@hotmail.se>
2025-03-31 14:43:17 +0200

    Reformats fishService and FishRepository to work with update method

--- src/main/java/org/example/fishtank/repository/FishRepository.java
+++ src/main/java/org/example/fishtank/repository/FishRepository.java
@@ -1,12 +1,19 @@
 package org.example.fishtank.repository;
 
 import org.example.fishtank.model.entity.Fish;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 
-
+@Repository
 public interface FishRepository extends ListCrudRepository<Fish, Integer> {
 
+    @Modifying
+    @Transactional
+    @Query("UPDATE Fish f SET f.name = :name, f.description = :description WHERE f.id = :id")
+    void update(String name, String description, int id);
 
-    void update(Fish oldFish);
 }
