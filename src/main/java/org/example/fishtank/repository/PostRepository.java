73fcc0714f9499f50b873320adfe631171bd4e45
Tova <tova.bryngelsson@live.se>
2025-03-31 15:27:41 +0200

    repositories but right this time

--- src/main/java/org/example/fishtank/repository/PostRepository.java
+++ src/main/java/org/example/fishtank/repository/PostRepository.java
@@ -1,7 +1,8 @@
 package org.example.fishtank.repository;
 
 import org.example.fishtank.model.entity.AppUser;
+import org.example.fishtank.model.entity.Post;
 import org.springframework.data.repository.ListCrudRepository;
 
-public interface PostRepository extends ListCrudRepository<AppUser, Integer> {
+public interface PostRepository extends ListCrudRepository<Post, Integer> {
 }
--- src/main/java/org/example/fishtank/repository/WaterTypeRepository.java
+++ src/main/java/org/example/fishtank/repository/WaterTypeRepository.java
@@ -1,7 +1,8 @@
 package org.example.fishtank.repository;
 
 import org.example.fishtank.model.entity.AppUser;
+import org.example.fishtank.model.entity.WaterType;
 import org.springframework.data.repository.ListCrudRepository;
 
-public interface WaterTypeRepository extends ListCrudRepository<AppUser, Integer> {
+public interface WaterTypeRepository extends ListCrudRepository<WaterType, Integer> {
 }
