46cefec52b833f9e260a7e6632748a114a50fd5a
Vilhelm Kennedal <vilhelm.kennedal@iths.se>
2025-03-31 15:01:09 +0200

    refractor once again

--- src/main/java/org/example/fishtank/service/AppUserService.java
+++ src/main/java/org/example/fishtank/service/AppUserService.java
@@ -13,6 +13,8 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Objects;
 
+import static org.example.fishtank.model.mapper.AppUserMapper.map;
+
 @Service
 @Transactional
 public class AppUserService {
@@ -51,8 +53,8 @@ public class AppUserService {
     public void update(int id, UpdateAppUser user) {
         AppUser existingUser = userRepository.findById(id).orElseThrow(() ->
                 new RuntimeException("User not found"));
-        AppUserMapper.map(user, existingUser);
-        userRepository.update(existingUser);
+        map(user, existingUser);
+        userRepository.update(existingUser.getName(),existingUser.getPasswordHash(), existingUser.getEmail(), existingUser.getAccess().toString());
     }
 }
 
