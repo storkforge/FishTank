07187db51638adb9e420edc81f8dc85acb1715d7
Tova <tova.bryngelsson@live.se>
2025-04-01 13:21:08 +0200

    updateFish test

--- src/test/java/org/example/fishtank/repository/FishRepositoryTest.java
+++ src/test/java/org/example/fishtank/repository/FishRepositoryTest.java
@@ -8,8 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -17,56 +15,61 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-
-
 @DataJpaTest
 @Testcontainers
+@Transactional
 class FishRepositoryTest {
 
     @Container
     @ServiceConnection
     static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:latest");
 
-    @Autowired FishRepository fishRepository;
-    @Autowired UserRepository userRepository;
-    @Autowired SexRepository sexRepository;
-    @Autowired WaterTypeRepository waterTypeRepository;
-    @Autowired AccessRepository accessRepository;
+    @Autowired
+    FishRepository fishRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    SexRepository sexRepository;
+    @Autowired
+    WaterTypeRepository waterTypeRepository;
+    @Autowired
+    AccessRepository accessRepository;
+    @Autowired
+    private EntityManager entityManager;
 
-    @Transactional
     @BeforeEach
     void beforeEach() {
-        try{
-        var access = new Access();
-        access.setName("Standard");
-        accessRepository.save(access);
+        try {
+            var access = new Access();
+            access.setName("Standard");
+            accessRepository.save(access);
 
 
-        var user = new AppUser();
-        user.setName("username");
-        user.setPasswordHash("password");
-        user.setEmail("username@email.com");
-        user.setAccess(access);
-        userRepository.save(user);
+            var user = new AppUser();
+            user.setName("username");
+            user.setPasswordHash("password");
+            user.setEmail("username@email.com");
+            user.setAccess(access);
+            userRepository.save(user);
 
-        var sex = new Sex();
-        sex.setName("Male");
-        sexRepository.save(sex);
+            var sex = new Sex();
+            sex.setName("Male");
+            sexRepository.save(sex);
 
-        var waterType = new WaterType();
-        waterType.setName("Salt water");
-        waterTypeRepository.save(waterType);
+            var waterType = new WaterType();
+            waterType.setName("Salt water");
+            waterTypeRepository.save(waterType);
 
-        var fish = new Fish();
-        fish.setName("Fish");
-        fish.setSpecies("Eel");
-        fish.setDescription("a fish");
-        fish.setSex(sex);
-        fish.setAppUser(user);
-        fish.setWaterType(waterType);
-        fishRepository.save(fish);
+            var fish = new Fish();
+            fish.setName("Fish");
+            fish.setSpecies("Eel");
+            fish.setDescription("a fish");
+            fish.setSex(sex);
+            fish.setAppUser(user);
+            fish.setWaterType(waterType);
+            fishRepository.save(fish);
 
-    } catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -78,5 +81,21 @@ class FishRepositoryTest {
         assertThat(result).hasSize(1);
     }
 
+    @Test
+    void updateFish() {
+        String newName = "New Fish Name";
+        String newDescription = "New Fish Description";
+        Fish fish = fishRepository.findAll().getFirst();
+        int fishId = fish.getId();
+
+        fishRepository.update(newName, newDescription, fishId);
+        entityManager.flush();
+        entityManager.clear();
+
+        Fish updatedFish = fishRepository.findById(fishId).orElse(null);
+        assertThat(updatedFish).isNotNull();
+        assertThat(updatedFish.getName()).isEqualTo(newName);
+        assertThat(updatedFish.getDescription()).isEqualTo(newDescription);
+    }
 
 }
