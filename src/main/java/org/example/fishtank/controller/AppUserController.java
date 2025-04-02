d9b2eba647ba260881ff49ed62708274b0c6708b
VictoriaNaenfeldtEklund <victoria.naenfeldt.eklund@iths.se>
2025-04-02 13:06:40 +0200

    Refactor base url to save logged in user.

--- src/main/java/org/example/fishtank/controller/AppUserController.java
+++ src/main/java/org/example/fishtank/controller/AppUserController.java
@@ -2,10 +2,8 @@ package org.example.fishtank.controller;
 
 import org.example.fishtank.service.AppUserService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -20,8 +18,8 @@ public class AppUserController {
     @GetMapping()
     public String base(@AuthenticationPrincipal OAuth2User oAuth2User){
 
-        var userAttributes = oAuth2User.getAttribute("login");
-        System.out.println(userAttributes);
+        String userNameAttribute = oAuth2User.getAttribute("login");
+        appUserService.save(userNameAttribute);
 
         return "redirect:/home";
     }
