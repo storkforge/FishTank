dfd9e2d54e8338ccd8cd17fef3083f0c2d0cc72a
VictoriaNaenfeldtEklund <victoria.naenfeldt.eklund@iths.se>
2025-04-02 13:03:13 +0200

    Refactor AppUserService to take Oauth2 username and save in database

--- src/main/java/org/example/fishtank/service/AppUserService.java
+++ src/main/java/org/example/fishtank/service/AppUserService.java
@@ -6,6 +6,7 @@ import org.example.fishtank.model.dto.appUserDto.ResponseAppUser;
 import org.example.fishtank.model.dto.appUserDto.UpdateAppUser;
 import org.example.fishtank.model.entity.Access;
 import org.example.fishtank.model.entity.AppUser;
+import org.example.fishtank.repository.AccessRepository;
 import org.example.fishtank.repository.UserRepository;
 import org.example.fishtank.model.mapper.AppUserMapper;
 import org.springframework.stereotype.Service;
@@ -19,15 +20,28 @@ import static org.example.fishtank.model.mapper.AppUserMapper.map;
 @Transactional
 public class AppUserService {
 
-    UserRepository userRepository;
+    private UserRepository userRepository;
+    private AccessRepository accessRepository;
 
-    public AppUserService(UserRepository userRepository) {
+    public AppUserService(UserRepository userRepository, AccessRepository accessRepository) {
         this.userRepository = userRepository;
+        this.accessRepository = accessRepository;
     }
 
-    public void save(CreateAppUser user) {
-        var newUser = map(user);
-        userRepository.save(newUser);
+    public void save(String oAuth2UserName){
+        CreateAppUser createAppUser = new CreateAppUser(oAuth2UserName, "email", "password", "Standard");
+        save(createAppUser);
+    }
+
+    public void save(CreateAppUser createAppUser) {
+
+        if(userRepository.findByName(createAppUser.name()) == null){
+            System.out.println("User is not found");
+            Access access = accessRepository.findByName(createAppUser.access());
+            var appUser = map(createAppUser, access);
+            userRepository.save(appUser);
+        }
+        System.out.println("User is found");
     }
 
     public ResponseAppUser findById(Integer id) {
@@ -50,10 +64,12 @@ public class AppUserService {
                 .orElse(null);
     }
 
-    public void update(int id, UpdateAppUser user) {
-        AppUser existingUser = userRepository.findById(id).orElseThrow(() ->
-                new RuntimeException("User not found"));
-        map(user, existingUser);
-        userRepository.update(existingUser.getName(),existingUser.getPasswordHash(), existingUser.getEmail(), existingUser.getAccess().toString());
-    }
+//    public void update(int id, UpdateAppUser user) {
+//        AppUser existingUser = userRepository.findById(id).orElseThrow(() ->
+//                new RuntimeException("User not found"));
+//        map(user, existingUser);
+//        userRepository.update(existingUser.getName(),existingUser.getPasswordHash(), existingUser.getEmail(), existingUser.getAccess().toString());
+//    }
 }
+
+
