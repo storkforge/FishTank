c12df367a88ded05f42f3f984f4f055487ab799f
Oskar <0skar01@hotmail.se>
2025-04-02 11:32:24 +0200

    75-create-postmapper (#81)
    * Adds PostMapper
    
    * Update PostRepository to work with PostService
    
    * Adds PostService
    
    * corrected CodeRabbits suggestions

--- src/main/java/org/example/fishtank/repository/PostRepository.java
+++ src/main/java/org/example/fishtank/repository/PostRepository.java
@@ -1,7 +1,14 @@
 package org.example.fishtank.repository;
 
 import org.example.fishtank.model.entity.Post;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface PostRepository extends ListCrudRepository<Post, Integer> {
+    @Modifying
+    @Transactional
+    @Query("UPDATE Post p SET p.text = :text  WHERE p.id = :id")
+    void update(String text, int id);
 }
