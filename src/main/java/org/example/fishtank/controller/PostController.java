2ad766d9c9ccff720f56c634a5fb86ef7bb07bd9
Oskar <0skar01@hotmail.se>
2025-04-03 13:59:01 +0200

    Adds forum html and PostController (#88)
    * Adds forum html and PostController
    
    * refactor to CodeRabbits suggestions
    
    * Remove check

---
+++ src/main/java/org/example/fishtank/controller/PostController.java
@@ -0,0 +1,36 @@
+package org.example.fishtank.controller;
+
+import org.example.fishtank.repository.FishRepository;
+import org.example.fishtank.repository.PostRepository;
+import org.example.fishtank.service.FishService;
+import org.example.fishtank.service.PostService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Objects;
+
+
+@Controller
+public class PostController {
+
+    private final PostService postService;
+    private final FishService fishService;
+
+    public PostController(PostService postService, FishService fishService) {
+        this.postService = postService;
+        this.fishService = fishService;
+    }
+
+    @GetMapping("/forum")
+    String forum(Model model) {
+        var postList = postService.getAllPost();
+        var fishList = postList.stream()
+                .map(post -> fishService.findById(post.fishId()))
+                .filter(Objects::nonNull)
+                .toList();
+        model.addAttribute("postList", postList);
+        model.addAttribute("fishList", fishList);
+        return "forum";
+    }
+}
