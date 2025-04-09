881aeed2d63f960500693524bfa98e90ecee8777
Oskar Dahlström <0skar01@hotmail.se>
2025-04-09 14:30:07 +0200

    Adds null checks to DTOs

--- src/main/java/org/example/fishtank/model/dto/fishDto/UpdateFish.java
+++ src/main/java/org/example/fishtank/model/dto/fishDto/UpdateFish.java
@@ -1,7 +1,11 @@
 package org.example.fishtank.model.dto.fishDto;
 
+import org.springframework.lang.NonNull;
+
 public record UpdateFish(
+        @NonNull
         String name,
+        @NonNull
         String description
 ) {
 }
