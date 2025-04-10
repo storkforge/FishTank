817c8e5de50ea2a6aa77463b1d4d07f11b3b6740
VictoriaNaenfeldtEklund <victoria.naenfeldt.eklund@iths.se>
2025-04-10 07:44:58 +0200

    Replace UserRepository with AppUserRepository to return Optional instead of Entity.

--- src/main/java/org/example/fishtank/repository/AppUserRepository.java
+++ src/main/java/org/example/fishtank/repository/AppUserRepository.java
@@ -1,13 +1,15 @@
 package org.example.fishtank.repository;
 
 import org.example.fishtank.model.entity.AppUser;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface AppUserRepository extends JpaRepository<AppUser, Integer> {
+public interface AppUserRepository extends ListCrudRepository<AppUser, Integer> {
 
     Optional<AppUser> findByName(String name);
+
+    Optional<AppUser> findByEmail(String email);
 }
--- src/main/java/org/example/fishtank/repository/UserRepository.java
+++ src/main/java/org/example/fishtank/repository/UserRepository.java
@@ -1,3 +1,4 @@
+/*
 package org.example.fishtank.repository;
 
 import jakarta.transaction.Transactional;
@@ -12,14 +13,10 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 public interface UserRepository extends ListCrudRepository<AppUser, Integer> {
-
-    AppUser findByEmail(String email);
-
     @Modifying
     @Transactional
     @Query("UPDATE AppUser u SET u.name = :name, u.passwordHash =:passwordHash, u.email = :email, u.access = :access WHERE u.id = :id")
     void update(@Param("name") String name, @Param("passwordHash") String passwordHash, @Param("email") String email, @Param("access") Access access);
 
-    @Query("select a from AppUser a where a.name = :name")
-    AppUser findByName(String name);
 }
+*/
