f8cc64e3d655ed7390f3c144617654a4c43743ed
VictoriaNaenfeldtEklund <victoria.naenfeldt.eklund@iths.se>
2025-04-10 07:45:59 +0200

    Refactor findByName to return Optional<Access> instead of entity.

--- src/main/java/org/example/fishtank/repository/AccessRepository.java
+++ src/main/java/org/example/fishtank/repository/AccessRepository.java
@@ -3,6 +3,9 @@ package org.example.fishtank.repository;
 import org.example.fishtank.model.entity.Access;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.Optional;
+
 public interface AccessRepository extends ListCrudRepository<Access, Integer> {
-    Access findByName(String name);
+
+    Optional<Access> findByName(String name);
 }
