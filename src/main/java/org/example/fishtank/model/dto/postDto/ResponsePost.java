2b1b2723c8e082534b61c74d2dcaeb710a5b56de
Vilhelm Kennedal <vilhelm.kennedal@iths.se>
2025-04-11 14:06:12 +0200

    add v10 and v11. Edit post mapper, and dto classes for location.

--- src/main/java/org/example/fishtank/model/dto/postDto/ResponsePost.java
+++ src/main/java/org/example/fishtank/model/dto/postDto/ResponsePost.java
@@ -5,6 +5,9 @@ import java.io.Serializable;
 public record ResponsePost(
         Integer id,
         String text,
+        String cityName,
+        Double longitude,
+        Double latitude,
         Integer fishId)
         implements Serializable {
 }
