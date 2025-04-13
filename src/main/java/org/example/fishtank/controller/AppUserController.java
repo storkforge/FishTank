4ceef98e1910b0e9067722e12d8ddd8c88326336
Tova <tova.bryngelsson@live.se>
2025-04-13 19:59:25 +0200

    User profile with access

--- src/main/java/org/example/fishtank/controller/AppUserController.java
+++ src/main/java/org/example/fishtank/controller/AppUserController.java
@@ -1,16 +1,34 @@
 package org.example.fishtank.controller;
 
+import org.example.fishtank.service.AppUserService;
 import org.example.fishtank.service.CurrentUser;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class  AppUserController {
+public class AppUserController {
+
+    private final CurrentUser currentUser;
+
+    public AppUserController(CurrentUser currentUser) {
+        this.currentUser = currentUser;
+
+    }
 
     @GetMapping("/home")
-    public String home(){
+    public String home() {
         return "home";
+    }
+
+    @GetMapping("/profile")
+    public String profile(Model model) {
+        var authentication = currentUser.getRole();
+        var user = currentUser.getUsername();
+        model.addAttribute("authentication", authentication);
+        model.addAttribute("user", user);
+        return "profile";
     }
 
 }
