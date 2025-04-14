d0e8f6766b627d38cd2e662d943e2ec010f0f5f2
Tova <tova.bryngelsson@live.se>
2025-04-14 16:23:18 +0200

    wisdoms for premium users

--- src/main/java/org/example/fishtank/controller/WisdomController.java
+++ src/main/java/org/example/fishtank/controller/WisdomController.java
@@ -1,11 +1,36 @@
 package org.example.fishtank.controller;
 
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
+@Controller
 public class WisdomController {
 
+    private final OpenAiChatModel chatModel;
+
+    @Autowired
+    public WisdomController(OpenAiChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
     @GetMapping("/wisdom")
-    public String getWisdom() {
+    public String generate(@RequestParam(value = "message", defaultValue = "Tell me a wisdom based on the category fish") String message, Model model) {
+
+        var message1 = new UserMessage(message);
+        var message2 = new SystemMessage("Answer short and concise. Don't start with any info about you being an ai. Start the answer directly.");
+        String response = chatModel.call(message1, message2);
+
+        model.addAttribute("generation", response);
+
         return "wisdom";
     }
 }
