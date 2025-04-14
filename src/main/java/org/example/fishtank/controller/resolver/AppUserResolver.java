38576ecf13719fc3b62e7632c6f37f7d659f07c5
Freddie Van Eijsden <freddievaneijsden@gmail.com>
2025-04-14 13:39:37 +0200

    add appuser queries

---
+++ src/main/java/org/example/fishtank/controller/resolver/AppUserResolver.java
@@ -0,0 +1,30 @@
+package org.example.fishtank.controller.resolver;
+
+import org.example.fishtank.model.entity.Access;
+import org.example.fishtank.model.entity.AppUser;
+import org.example.fishtank.repository.AppUserRepository;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class AppUserResolver {
+
+    AppUserRepository appUserRepository;
+
+    public AppUserResolver(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
+
+    @QueryMapping
+    public List<AppUser> getAllAppUser() {
+        return appUserRepository.findAll();
+    }
+
+    @QueryMapping
+    public AppUser getAppUserByName(@Argument String name) {
+        return  appUserRepository.findByName(name).orElse(null);
+    }
+}
