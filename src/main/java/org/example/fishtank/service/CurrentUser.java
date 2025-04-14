ed63fed2f66178f86aedae795e5bc8516a33e795
Tova Bryngelsson <tova.bryngelsson@live.se>
2025-04-14 11:35:47 +0200

    131 implement authorization benefits (#132)
    * User profile with access
    
    * home screen updated for specific user
    
    * standard dolphin and premium dolphin picture

--- src/main/java/org/example/fishtank/service/CurrentUser.java
+++ src/main/java/org/example/fishtank/service/CurrentUser.java
@@ -1,11 +1,34 @@
 package org.example.fishtank.service;
 
+import org.example.fishtank.repository.AppUserRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
 
+@Service
 public final class CurrentUser {
+
+    private final AppUserRepository appUserRepository;
+
+    public CurrentUser(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
 
     public static int getId(){
         String userIdString = SecurityContextHolder.getContext().getAuthentication().getName();
         return Integer.parseInt(userIdString);
+    }
+
+    public String getUsername(){
+        var id = SecurityContextHolder.getContext().getAuthentication().getName();
+        return appUserRepository.findById(Integer.parseInt(id))
+                .orElseThrow(() -> new RuntimeException("User not found"))
+                .getName();
+    }
+
+    public String getRole(){
+        var id = SecurityContextHolder.getContext().getAuthentication().getName();
+        return appUserRepository.findById(Integer.parseInt(id))
+                .orElseThrow(() -> new RuntimeException("User not found"))
+                .getAccess().getName();
     }
 }
