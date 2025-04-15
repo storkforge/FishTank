5feededa07b62716bf0ea14b82fef4f86b9fb4a2
VictoriaNaenfeldtEklund <victoria.naenfeldt.eklund@iths.se>
2025-04-15 16:55:05 +0200

    Refactor UpdateAppUser to be able to take null values + not be able to take access as access will be set on its own.

--- src/main/java/org/example/fishtank/model/dto/appUserDto/UpdateAppUser.java
+++ src/main/java/org/example/fishtank/model/dto/appUserDto/UpdateAppUser.java
@@ -1,14 +1,7 @@
 package org.example.fishtank.model.dto.appUserDto;
 
-import org.springframework.lang.NonNull;
-
 public record UpdateAppUser(
-        @NonNull
         String name,
-        @NonNull
         String email,
-        @NonNull
-        String passwordHash,
-        @NonNull
-        String access) {
+        String password) {
 }
