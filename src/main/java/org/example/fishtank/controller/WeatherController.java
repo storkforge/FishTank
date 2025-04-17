fdafcb6386fb95222c1621c98a710158018d09c4
jesper <Jesper.Sandklef@iths.se>
2025-04-17 10:44:31 +0200

    updated weatherController to let user decide location

--- src/main/java/org/example/fishtank/controller/WeatherController.java
+++ src/main/java/org/example/fishtank/controller/WeatherController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
@@ -11,11 +13,10 @@ import java.util.Map;
 @Controller
 public class WeatherController {
 
-    private final String API_URL = "https://api.weatherapi.com/v1/current.json?key=25c23807bbdb4d399b391221251604&q={location}";
 
-    @GetMapping("/weather/{location}")
+    @GetMapping("/weather")
 
-    public String getWeather(@PathVariable String location, Model model) {
+    public String getWeather(@RequestParam(required = false) String location, Model model) {
 
         String apiUrl = "https://api.weatherapi.com/v1/current.json?key=25c23807bbdb4d399b391221251604&q=" + location;
 
@@ -25,6 +26,7 @@ public class WeatherController {
 
         if (response != null && response.get("current") != null) {
             Map<String, Object> current = (Map<String, Object>) response.get("current");
+            model.addAttribute("location", location);
             model.addAttribute("temperature", current.get("temp_c"));
             model.addAttribute("condition", ((Map<String, Object>) current.get("condition")).get("text"));
         }
@@ -32,6 +34,8 @@ public class WeatherController {
         return "weather";
     }
 }
+
+
 
 
 
