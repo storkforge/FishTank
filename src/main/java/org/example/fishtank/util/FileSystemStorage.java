37df1d4279fe61a417e6f32339e422d118d57c49
Freddievaneijsden <166924808+Freddievaneijsden@users.noreply.github.com>
2025-04-17 10:37:47 +0200

    Refactor image service and add tests (#152)
    * refactor ImageService
    
    * add prepareUploadDirectory test
    
    * add sanitized test
    
    * add generateUniqueFilenName test
    
    * add buildSafeFilePath test
    
    * add saveImage test
    
    * remove beforeEach
    
    * update prepareUploadDirectory test

---
+++ src/main/java/org/example/fishtank/util/FileSystemStorage.java
@@ -0,0 +1,16 @@
+package org.example.fishtank.util;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Service
+public class FileSystemStorage implements FileStorage{
+    @Override
+    public void save(Path path, MultipartFile file) throws IOException {
+        Files.copy(file.getInputStream(), path);
+    }
+}
