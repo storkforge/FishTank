f96312b36d57579e8d3bdee8171c8e6f8c432d9a
VictoriaNaenfeldtEklund <victoria.naenfeldt.eklund@iths.se>
2025-04-19 07:57:27 +0200

    Remove unused imports

--- src/test/java/org/example/fishtank/service/AppUserServiceTest.java
+++ src/test/java/org/example/fishtank/service/AppUserServiceTest.java
@@ -18,8 +18,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
