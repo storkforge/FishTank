839eea4399fc9b21a3adbb9f1babeb24d649e372
Vilhelm Kennedal <vilhelm.kennedal@iths.se>
2025-04-21 16:32:32 +0200

    change an import

--- src/test/java/org/example/fishtank/controller/FishControllerTest.java
+++ src/test/java/org/example/fishtank/controller/FishControllerTest.java
@@ -22,7 +22,7 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.*;
