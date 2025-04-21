edceab98af3dd360408366881a061974be06b43f
Freddievaneijsden <166924808+Freddievaneijsden@users.noreply.github.com>
2025-04-21 11:31:46 +0200

    add FileSystemStorageTest and update save() (#166)

--- src/main/java/org/example/fishtank/util/FileSystemStorage.java
+++ src/main/java/org/example/fishtank/util/FileSystemStorage.java
@@ -6,11 +6,12 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 @Service
 public class FileSystemStorage implements FileStorage{
     @Override
     public void save(Path path, MultipartFile file) throws IOException {
-        Files.copy(file.getInputStream(), path);
+        Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
     }
 }
